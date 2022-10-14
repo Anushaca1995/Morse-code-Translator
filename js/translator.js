730a0e0f797e4bea84d1cc6a66a4498a871ab885
Anusha Antony <anushaca1995@gmail.com>
2022-10-14 16:18:46 +0100

    Added keypress and translate functionalities

--- js/translator.js
+++ js/translator.js
@@ -38,6 +38,48 @@ const morseData = {
     0: "-----",
 };
 
+export const ValidateData = (input) => {
+    let returnVal;
+    let charCode;
+    if ((typeof input) == "object") {
+        charCode = input.keyCode;
+        console.log(charCode);
+        returnVal=getValid(charCode);
+    } else {
+        let returnStr;
+        for (let index=0;index<input.length;index++){
+            charCode = input.charCodeAt(index);
+            returnStr=getValid(charCode);
+            if (!returnStr){
+                return false;
+            }
+        }
+        returnVal=returnStr;
+    }
+    if (!returnVal){
+        input.preventDefault();
+    }
+}
+
+export const getValid = (charCode) =>{
+    if (charCode > 32 && (charCode < 65 || charCode > 90) &&
+            (charCode < 97 || charCode > 122)) {
+            if (charCode > 47 && charCode < 58) {
+                return true;
+            }
+            return false;
+        }
+    return true;
+}
+
+export const getMorse = () => {
+    let text = document.querySelector(".text-input").value;
+    const translate=document.querySelector(".translator");
+    translate.innerHTML=translateArray(text);
+    console.log(translateArray(text));
+    
+}
+
 export const translateArray = (str) => {
     let morseString = "";
     for (let index=0;index<str.length;index++){
@@ -48,4 +90,3 @@ export const translateArray = (str) => {
     }
     return morseString;
 }
-
