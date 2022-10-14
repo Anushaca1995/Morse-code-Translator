e157d42dffa871f20189cf588cf81fb151889c36
Anusha Antony <anushaca1995@gmail.com>
2022-10-14 15:47:39 +0100

    Added functionality to check given is valid

---
+++ js/main.js
@@ -0,0 +1,99 @@
+//import {} from "./translator.js";
+const morseData = {
+    " ": "|",
+    A: ".-",
+    B: "-...",
+    C: "-.-.",
+    D: "-..",
+    E: ".",
+    F: "..-.",
+    G: "--.",
+    H: "....",
+    I: "..",
+    J: ".---",
+    K: "-.-",
+    L: ".-..",
+    M: "--",
+    N: "-.",
+    O: "---",
+    P: ".--.",
+    Q: "--.-",
+    R: ".-.",
+    S: "...",
+    T: "-",
+    U: "..-",
+    V: "...-",
+    W: ".--",
+    X: "-..-",
+    Y: "-.--",
+    Z: "--..",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    0: "-----",
+};
+
+const ValidateData = (input) => {
+    let returnVal;
+    let charCode;
+    if ((typeof input) == "object") {
+        charCode = input.keyCode;
+        returnVal=getValid(charCode);
+    } else {
+        let returnStr;
+        for (let index=0;index<input.length;index++){
+            charCode = input.charCodeAt(index);
+            returnStr=getValid(charCode);
+            if (!returnStr){
+                return false;
+            }
+        }
+        returnVal=returnStr;
+    }
+    return returnVal;
+}
+
+const getValid = (charCode) =>{
+    if (charCode > 32 && (charCode < 65 || charCode > 90) &&
+            (charCode < 97 || charCode > 122)) {
+            if (charCode > 47 && charCode < 58) {
+                return true;
+            }
+            return false;
+        }
+    return true;
+}
+
+
+const getMorse = () => {
+    let text = document.querySelector(".text-input").value;
+    const translate=document.querySelector(".translator");
+    translate.innerHTML=translateArray(text);
+    console.log(translateArray(text));
+    
+}
+
+
+const morseButton=document.querySelector(".morse-button");
+morseButton.addEventListener("click", getMorse);
+
+const translateArray = (str) => {
+    let morseString = "";
+    for (let index=0;index<str.length;index++){
+        let word=str[index];
+        word.trim();
+        word=word.toUpperCase();
+        morseString+=morseData[word];
+    }
+    return morseString;
+}
+
+
+
+
