ad638a4a650f6945d46cb18e3d72fb8c332fa0b6
Anusha Antony <anushaca1995@gmail.com>
2022-10-14 15:49:48 +0100

    Added morse code functionality in translator

--- js/translator.js
+++ js/translator.js
@@ -1,2 +1,51 @@
-import * as main from './main.js';
-import {morseData} from './morseCodeData.js';
+const morseData = {
+    " ": "|",
+    A: ".-",
+    B: "-...",
+    C: "-.-.",
+    D: "-..",
+    E: ".",
+    F: "..-.",
+    G: "--.",
+    H: "....",
+    I: "..",
+    J: ".---",
+    K: "-.-",
+    L: ".-..",
+    M: "--",
+    N: "-.",
+    O: "---",
+    P: ".--.",
+    Q: "--.-",
+    R: ".-.",
+    S: "...",
+    T: "-",
+    U: "..-",
+    V: "...-",
+    W: ".--",
+    X: "-..-",
+    Y: "-.--",
+    Z: "--..",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    0: "-----",
+};
+
+export const translateArray = (str) => {
+    let morseString = "";
+    for (let index=0;index<str.length;index++){
+        let word=str[index];
+        word.trim();
+        word=word.toUpperCase();
+        morseString+=morseData[word];
+    }
+    return morseString;
+}
+
